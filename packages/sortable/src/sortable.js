d4917fadea14f5d579d872dab47805c27d5140fb
liyatang <liyatang@qq.com>
2020-01-10 23:01:33 +0800

    Feature sortable group (#9)
    * sortable group
    
    * sortable group
    
    * sortable group

--- packages/sortable/src/sortable.js
+++ packages/sortable/src/sortable.js
@@ -3,22 +3,43 @@ import PropTypes from 'prop-types'
 import SortableBase from './base'
 import _ from 'lodash'
 import classNames from 'classnames'
+import { devWarnForHook, warn } from 'gm-util'
 
 const Sortable = ({
   data,
   onChange,
+  groupValues,
   renderItem,
   tag,
-  options = {},
+  options,
   ...rest
 }) => {
+  devWarnForHook(() => {
+    if (groupValues && !options.group) {
+      warn('groupValues need options.group')
+    }
+  })
+
+  let filterData = data
+
+  if (groupValues) {
+    filterData = _.filter(data, v => groupValues.includes(v.value))
+  }
+
   const handleChange = order => {
     order = _.map(order, v => JSON.parse(v))
-    const newData = _.sortBy(data.slice(), v => order.indexOf(v.value))
+
+    // 打平
+    const map = {}
+    _.each(data, v => {
+      map[v.value] = v
+    })
+
+    const newData = _.map(order, v => map[v])
     onChange(newData)
   }
 
-  const items = _.map(data, (v, index) => (
+  const items = _.map(filterData, (v, index) => (
     <div
       key={v.value}
       data-id={JSON.stringify(v.value)}
@@ -48,7 +69,9 @@ const Sortable = ({
 Sortable.propTypes = {
   /** [{value, text, ...}, {value, text, ...}] */
   data: PropTypes.array.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
+  // options.group 有值的时候要传。此时的 data 是 group 集合数据，groupValues 是当前组件的数据
+  groupValues: PropTypes.array,
   renderItem: PropTypes.func,
   /** 支持 ref */
   tag: PropTypes.node,
@@ -56,7 +79,8 @@ Sortable.propTypes = {
 }
 
 Sortable.defaultProps = {
-  renderItem: item => item.text
+  renderItem: item => item.text,
+  options: {}
 }
 
 export default Sortable
