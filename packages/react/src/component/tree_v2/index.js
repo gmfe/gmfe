85ab87489ba26d4282773aa06da929fa9b43e21a
hejunze <hejunze@guanmai.cn>
2020-03-09 15:20:41 +0800

    update: 更新treev2的样式和逻辑。

--- packages/react/src/component/tree_v2/index.js
+++ packages/react/src/component/tree_v2/index.js
@@ -1,8 +1,15 @@
 import { getLocale } from '@gmfe/locales'
 import React, { useEffect, useRef, useState, useMemo } from 'react'
 import PropTypes from 'prop-types'
+import Input from '../input'
+import Button from '../button'
 import Flex from '../flex'
-import { getLeafValues, getUnLeafValues, filterWithQuery } from './util'
+import {
+  getLeafValues,
+  getUnLeafValues,
+  filterWithQuery,
+  getItemOffsetHeight
+} from './util'
 import { filterGroupListLeaf } from '../../common/util'
 import _ from 'lodash'
 import classNames from 'classnames'
@@ -35,13 +42,26 @@ const TreeV2 = ({
   renderLeafItem,
   renderGroupItem,
   className,
+  showAllCheck,
+  indeterminateList,
+  activeValue,
+  onActiveValues,
+  withFindFilter,
+  findPlaceholder,
   ...rest
 }) => {
   const refList = useRef(null)
+  const refFixedList = useRef(null)
+  const refInput = useRef(null)
   const [listHeight, setListHeight] = useState(null)
 
+  // 搜索
   const [query, setQuery] = useState('')
   const [delayQuery, setDelayQuery] = useState('')
+  // 定位
+  const [findQuery, setFindQuery] = useState('')
+  const [findItem, setFindItem] = useState([])
+  const [findIndex, setFindIndex] = useState(-1)
   // 区分正常的 展开收起 和 搜索导致的展开收起
   const [groupSelected, setGroupSelected] = useState([])
   // 保存一个函数的引用而已
@@ -62,6 +82,10 @@ const TreeV2 = ({
   useEffect(() => {
     setListHeight(refList.current.offsetHeight)
   }, [])
+  useEffect(() => {
+    const scroll_height = findItem[findIndex] ? findItem[findIndex].height : 0
+    findItem[findIndex] && handleScroll(scroll_height)
+  }, [findIndex])
 
   const handleSelectAll = checked => {
     onSelectValues(checked ? getLeafValues(list) : [])
@@ -73,6 +97,44 @@ const TreeV2 = ({
 
     // 延迟更新 delayQuery
     refDebounce.current(query)
+  }
+
+  const handleFindQuery = e => {
+    const v = e.target.value
+    setFindQuery(v)
+    setFindItem([])
+    setFindIndex(-1)
+  }
+
+  const handleScroll = height => {
+    // 全部展开，方便控制scroll
+    setGroupSelected(getUnLeafValues(list))
+    refFixedList.current.scrollTo(height)
+  }
+
+  const handleSearch = () => {
+    const find_list = withFindFilter(list, findQuery)
+    const box_height = refList.current.offsetHeight
+    const scroll_list = _.map(find_list, item => ({
+      height: getItemOffsetHeight(item, 28, box_height, list),
+      value: item.value
+    }))
+
+    if (!find_list || !scroll_list.length) {
+      handleScroll(0)
+      return
+    }
+    setFindItem(scroll_list)
+    return scroll_list
+  }
+
+  const handleNext = () => {
+    const list = handleSearch()
+    if (findIndex + 1 === list.length) {
+      setFindIndex(0)
+    } else {
+      setFindIndex(findIndex + 1)
+    }
   }
 
   const handleGroupSelect = groupSelected => {
@@ -99,9 +161,24 @@ const TreeV2 = ({
           />
         </div>
       )}
+      {withFindFilter && (
+        <Flex>
+          <Input
+            ref={refInput}
+            placeholder={findPlaceholder}
+            onChange={handleFindQuery}
+            value={findQuery}
+            className='form-control'
+          />
+          <Button type='primary' onClick={handleNext}>
+            {getLocale('定位')}
+          </Button>
+        </Flex>
+      )}
       <div className='gm-flex-flex' ref={refList}>
         {!!listHeight && (
           <List
+            listRef={refFixedList}
             list={filterList}
             listHeight={listHeight}
             groupSelected={newGS}
@@ -110,15 +187,22 @@ const TreeV2 = ({
             onSelectValues={onSelectValues}
             renderLeafItem={renderLeafItem}
             renderGroupItem={renderGroupItem}
+            onActiveValues={onActiveValues}
+            indeterminateList={indeterminateList}
+            activeValue={
+              findItem[findIndex] ? findItem[findIndex].value : activeValue
+            }
           />
         )}
       </div>
 
-      <Bottom
-        list={list}
-        selectedValues={selectedValues}
-        onChange={handleSelectAll}
-      />
+      {showAllCheck ? (
+        <Bottom
+          list={list}
+          selectedValues={selectedValues}
+          onChange={handleSelectAll}
+        />
+      ) : null}
     </Flex>
   )
 }
@@ -138,6 +222,8 @@ TreeV2.propTypes = {
   list: PropTypes.array.isRequired,
   selectedValues: PropTypes.array.isRequired,
   onSelectValues: PropTypes.func.isRequired,
+  onActiveValues: PropTypes.func,
+  activeValue: PropTypes.string,
 
   title: PropTypes.string,
   /** 过滤函数，默认自带，不需要就 false */
@@ -145,6 +231,13 @@ TreeV2.propTypes = {
   renderLeafItem: PropTypes.func,
   renderGroupItem: PropTypes.func,
   placeholder: PropTypes.string,
+  /** 全选开关是否显示 */
+  showAllCheck: PropTypes.bool,
+  /** 半勾选状态，数组 [value] */
+  indeterminateList: PropTypes.array,
+  /** 定位过滤函数, 默认自带，不需要就 false */
+  withFindFilter: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
+  findPlaceholder: PropTypes.string,
 
   className: PropTypes.string,
   style: PropTypes.object
@@ -152,7 +245,13 @@ TreeV2.propTypes = {
 
 TreeV2.defaultProps = {
   withFilter: true,
-  placeholder: getLocale('搜索')
+  withFindFilter: false,
+  showAllCheck: true,
+  activeValue: null,
+  placeholder: getLocale('搜索'),
+  findPlaceholder: getLocale('输入定位信息'),
+  onActiveValues: () => [],
+  indeterminateList: []
 }
 
 export default TreeV2
