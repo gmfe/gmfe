3dc0f7bd4bf899e9f969b9d99e926c88bb0bcdbf
zhongsink <zhongink@gmail.com>
2020-03-11 17:15:42 +0800

    qinniu

--- packages/qiniu-image/src/upload.js
+++ packages/qiniu-image/src/upload.js
@@ -1,24 +1,29 @@
-import { getUploadImageName, getToken, request, config } from './util'
-
-async function uploadImage(blob) {
+import { getUploadImageName, getToken, request, optionsDefault } from './util'
+/**
+ *
+ * @param {*} blob Blob
+ * @param {*} options optionsDefault
+ */
+async function uploadImage(blob, opts = {}) {
+  const options = { ...optionsDefault, ...opts }
   if (!blob.type.startsWith('image/')) {
     throw new Error('need image')
   }
 
-  const token = await getToken()
+  const token = await getToken(options)
 
   const name = getUploadImageName(blob)
-  const path = `product_image/${name}`
+  const path = options.prefix ? `${options.prefix}/${name}` : name
 
-  await request(config.uploadUrl, {
+  await request(options.uploadUrl, {
     file: blob,
     token,
     key: path
   })
 
-  const url = `${config.domain}/${path}`
+  const url = `${options.domain}/${path}`
 
-  return url
+  return { url, key: path }
 }
 
 export default uploadImage
