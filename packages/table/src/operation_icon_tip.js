061decd287eacb2646a8eb872894c33717d19cbc
LaamGinghong <351390560@qq.com>
2020-03-19 18:31:39 +0800

    fixed

--- packages/table/src/operation_icon_tip.js
+++ packages/table/src/operation_icon_tip.js
@@ -9,7 +9,7 @@ const Tip = styled.div`
 
 const OperationIconTip = ({ children, tip }, ref) => {
   return (
-    <ToolTip showArrow popup={<Tip>{tip}</Tip>} ref={ref}>
+    <ToolTip center showArrow popup={<Tip>{tip}</Tip>} ref={ref}>
       {children}
     </ToolTip>
   )
