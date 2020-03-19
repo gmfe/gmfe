9e13cf63d7cd669190b61d1d6dcea95ffe9d4955
tubulang <1053736438standard@gmail.com>
2020-03-19 12:00:43 +0800

    fix form memo

--- packages/react/src/component/form/form_group.js
+++ packages/react/src/component/form/form_group.js
@@ -1,10 +1,36 @@
 import { getLocale } from '@gmfe/locales'
-import React, { useMemo, useRef, useEffect, useState } from 'react'
+import React, { useMemo, useRef, useEffect, useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import Affix from '../affix'
 import _ from 'lodash'
 import Button from '../button'
 import { warn, devWarnForHook } from 'gm-util'
+
+const Action = ({ onCancel, onSubmit, disabled, saveText, actions }) => {
+  return (
+    <>
+      {onCancel && (
+        <>
+          <Button onClick={onCancel}>{getLocale('取消')}</Button>
+          <div className='gm-gap-10' />
+        </>
+      )}
+      <Button type='primary' disabled={disabled} onClick={onSubmit}>
+        {saveText}
+      </Button>
+      {actions}
+    </>
+  )
+}
+
+Action.propTypes = {
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func,
+  disabled: PropTypes.bool,
+  /** save按钮文案，默认保存 */
+  saveText: PropTypes.string,
+  actions: PropTypes.element
+}
 
 /** 聚合多个表单，统一处理 submit */
 const FormGroup = ({
@@ -43,39 +69,38 @@ const FormGroup = ({
     }
   }, [])
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    onSubmit && onSubmit()
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault()
+      onSubmit && onSubmit()
 
-    let isPass = true
+      let isPass = true
 
-    _.each(formRefs, form => {
-      if (!form.current.apiDoValidate()) {
-        isPass = false
+      _.each(formRefs, form => {
+        if (!form.current.apiDoValidate()) {
+          isPass = false
+        }
+      })
+
+      if (isPass) {
+        onSubmitValidated && onSubmitValidated()
       }
-    })
+    },
+    [onSubmit, formRefs, onSubmitValidated]
+  )
 
-    if (isPass) {
-      onSubmitValidated && onSubmitValidated()
-    }
-  }
-
+  // 为了内部引用值引起维护方的注意，故抽出纯函数的形式来使用,但是不知道这里为什么要memo
   const action = useMemo(
     () => (
-      <>
-        {onCancel && (
-          <>
-            <Button onClick={onCancel}>{getLocale('取消')}</Button>
-            <div className='gm-gap-10' />
-          </>
-        )}
-        <Button type='primary' disabled={disabled} onClick={handleSubmit}>
-          {saveText}
-        </Button>
-        {actions}
-      </>
+      <Action
+        disabled={disabled}
+        actions={actions}
+        onSubmit={handleSubmit}
+        saveText={saveText}
+        onCancel={onCancel}
+      />
     ),
-    [disabled, actions]
+    [disabled, actions, handleSubmit, saveText, onCancel]
   )
 
   useEffect(() => {
