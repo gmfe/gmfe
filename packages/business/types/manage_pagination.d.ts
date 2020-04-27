e3660793c7b8edf24b02a92b5056224ef98ad255
LaamGinghong <351390560@qq.com>
2020-04-27 17:21:30 +0800

    build: business declaration file

---
+++ packages/business/types/manage_pagination.d.ts
@@ -0,0 +1,37 @@
+import { Component, ReactNode } from 'react'
+import {
+  ManagePaginationOptions,
+  ManagePaginationPromiseResult,
+} from './common'
+
+interface ManagePaginationProps {
+  id?: string
+  onRequest(
+    pagination: ManagePaginationOptions
+  ): Promise<ManagePaginationPromiseResult>
+  defaultLimit?: number
+  children: ReactNode
+}
+
+interface ManagePaginationState {
+  limit: number
+  offset: number
+  count: number
+
+  nextDisabled: boolean
+  loading: boolean
+}
+
+declare class ManagePagination extends Component<
+  ManagePaginationProps,
+  ManagePaginationState
+> {
+  static defaultProps: {
+    defaultLimit: number
+  }
+
+  public apiDoFirstRequest(params?: { [key: string]: any }): void
+  public apiDoCurrentRequest(): void
+}
+
+export default ManagePagination
