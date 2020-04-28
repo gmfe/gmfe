1dd25bd4c8822c48e9a51890cb3475e1584901b8
LaamGinghong <351390560@qq.com>
2020-04-28 15:15:41 +0800

    build: table-x declaration file

---
+++ packages/table-x/types/base.d.ts
@@ -0,0 +1,23 @@
+import { CSSProperties, PropsWithChildren, ReactElement, UIEvent } from 'react'
+import { Column } from 'react-table'
+
+interface TableXProps<Original extends { [key: string]: any }> {
+  columns: Column<Original>[]
+  data: Original[]
+  loading?: boolean
+  /* 由其他 hoc 传下来 */
+  keyField?: string
+  /* table是否平铺 */
+  tiled?: boolean
+  /* 当前行是否disable */
+  isTrDisable?(value: Original): boolean
+  /* 当前行是否高亮 */
+  isTrHighlight?(value: Original): boolean
+  onScroll?(event: UIEvent<HTMLDivElement>): void
+  className?: string
+  style?: CSSProperties
+}
+
+declare const TableX: <P>(
+  props: PropsWithChildren<TableXProps<P>>
+) => ReactElement
---
+++ packages/table-x/types/index.d.ts
@@ -0,0 +1,1 @@
+export { default as TableX } from './base'
