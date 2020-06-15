9283a20b28d84415870b9c96fb0b28a9c77a6af9
linxueyu <lin2006yuo@gmail.com>
2020-06-15 20:04:58 +0800

    select all

--- packages/react/src/component/tree_v2/index.js
+++ packages/react/src/component/tree_v2/index.js
@@ -89,7 +89,7 @@ const TreeV2 = ({
   }, [findIndex])
 
   const handleSelectAll = checked => {
-    onSelectValues(checked ? getLeafValues(filterList) : [])
+    onSelectValues(checked ? getLeafValues(list) : [])
   }
 
   const handleQuery = e => {
@@ -201,7 +201,7 @@ const TreeV2 = ({
 
       {showAllCheck ? (
         <Bottom
-          list={filterList}
+          list={list}
           selectedValues={selectedValues}
           onChange={handleSelectAll}
         />
