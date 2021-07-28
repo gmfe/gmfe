ba310938f78e28d4dd9b702fd21dc0660150172d
yuyanmin <1812006192@qq.com>
2021-07-28 10:13:37 +0800

    feat: add sorttable

---
+++ packages/table/src/hoc/sort_table_table.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import TableX from '../base'
+import SortableJS from 'sortablejs'
+import _ from 'lodash'
+
+function sortableTable(Component) {
+  const SortableTable = ({ id, data, onSortChange, keyField, ...rest }) => {
+    id = id || 'id' + +new Date() + '' + String(Math.random()).slice(2)
+
+    useEffect(() => {
+      const target = document.querySelector(`#${id} .rt-tbody`)
+
+      const sortable = new SortableJS(target, {
+        animation: 150,
+        onStart: () => {
+          target.classList.add('gm-table-x-sortable-active')
+        },
+        onEnd: () => {
+          target.classList.remove('gm-table-x-sortable-active')
+        },
+        onUpdate: () => {
+          const newIds = sortable.toArray()
+          const newData = _.sortBy(data.slice(), v =>
+            newIds.indexOf(v[keyField])
+          )
+          onSortChange(newData)
+        }
+      })
+
+      return () => {
+        sortable.destroy()
+      }
+    }, [data])
+
+    return <Component {...rest} id={id} data={data} keyField={keyField} />
+  }
+
+  SortableTable.propTypes = {
+    ...TableX.propTypes,
+
+    keyField: PropTypes.string,
+    onSortChange: PropTypes.func.isRequired
+  }
+
+  SortableTable.defaultProps = {
+    keyField: 'value'
+  }
+
+  return SortableTable
+}
+
+export default sortableTable
